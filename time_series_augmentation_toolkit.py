97ffc186c0d47cf75da25c3b0c69726c2b002eb4
Philip Wang <RainBoltz@users.noreply.github.com>
2019-05-23 01:04:40 +0800

    add DN method
    new_value = alpha * delta + original_value

--- time_series_augmentation_toolkit.py
+++ time_series_augmentation_toolkit.py
@@ -1,38 +1,55 @@
 import numpy as np
 
 def DN(ts, sigma=0.0, mu=1.0, alpha=1.0, random_seed=None):
-'''
-ts: np.array, time series data
-sigma: float, standard deviation of the noise distribution
-mu: float, mean of the noise distribution
-alpha: float, the ratio of changes (new_value = alpha * delta + original_value)
-random_seed: int, set numpy random seed
-'''
+    '''
+    ts: np.array, time series data
+    sigma: float, standard deviation of the noise distribution
+    mu: float, mean of the noise distribution
+    alpha: float, the ratio of changes (new_value = alpha * delta + original_value)
+    random_seed: int, set numpy random seed
+    '''
+    if random_seed:
+        np.random.seed(random_seed)
+    
+    ts_length = ts.shape[0]
+    Ndist = sigma * np.random.randn(ts_length) + mu
+    for i,v in enumerate(ts):
+        ts[i] = alpha * Ndist[i] + v #new_value = alpha * delta + original_value
+        
     return ts
 
 def WS(ts_set, n=None, random_seed=None):
-'''
-ts_set: np.ndarray, time series dataset
-slicing_size: int, size of the sliced series, if None then default to 1/4 data length
-random_seed: int, set numpy random seed
-'''
+    '''
+    ts_set: np.ndarray, time series dataset
+    slicing_size: int, size of the sliced series, if None then default to 1/4 data length
+    random_seed: int, set numpy random seed
+    '''
+    if random_seed:
+        np.random.seed(random_seed)
+        
     return ts_set
 
 def WW(ts, n=None, alpha=0.5, random_seed=None):
-'''
-ts: np.array, time series data
-n: int, influence range (+/-n values), if None then default to 1
-alpha: float, the ratio of streching
-random_seed: int, set numpy random seed
-'''
+    '''
+    ts: np.array, time series data
+    n: int, influence range (+/-n values), if None then default to 1
+    alpha: float, the ratio of streching
+    random_seed: int, set numpy random seed
+    '''
+    if random_seed:
+        np.random.seed(random_seed)
+        
     return ts
 
 def DM(ts_set, random_seed=None):
-'''
-ts_set: np.ndarray, time series dataset
+    '''
+    ts_set: np.ndarray, time series dataset
 
-//TODO
+    //TODO
 
-random_seed: int, set numpy random seed
-'''
+    random_seed: int, set numpy random seed
+    '''
+    if random_seed:
+        np.random.seed(random_seed)
+        
     return ts_set
